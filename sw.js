4e90b61e8664414298baeb225efa21d19c756bec
Saitama21 <62865879+Saitama21@users.noreply.github.com>
2026-09-21 20:07:43 +0300

    chore: refresh cache for dock viewport fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="828d-modes-v0.3.1";
+const CACHE="828d-modes-v0.3.2";
 const CORE=[
   "./","./index.html","./styles.css","./app.js","./manifest.webmanifest",
   "./assets/icon.svg","./assets/flange.svg","./assets/bushing.svg",
