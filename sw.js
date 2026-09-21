96dd7e72a164ab3d9478421b429e603eb436ea43
Saitama21 <62865879+Saitama21@users.noreply.github.com>
2026-09-21 20:12:07 +0300

    chore: refresh cache for 828-test dock contract

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="828d-modes-v0.3.2";
+const CACHE="828d-modes-v0.3.3";
 const CORE=[
   "./","./index.html","./styles.css","./app.js","./manifest.webmanifest",
   "./assets/icon.svg","./assets/flange.svg","./assets/bushing.svg",
