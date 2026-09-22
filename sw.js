bf009b8ad21cc0d42c0009e2c7cce6b163c8694c
Saitama21 <62865879+Saitama21@users.noreply.github.com>
2026-09-22 20:00:45 +0300

    chore: bump dock visual cache v0.5.1

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="828d-modes-v0.5.0";
+const CACHE="828d-modes-v0.5.1";
 const CORE=[
   "./","./index.html","./styles.css","./app.js","./manifest.webmanifest",
   "./assets/icon.svg","./assets/flange.svg","./assets/bushing.svg",
