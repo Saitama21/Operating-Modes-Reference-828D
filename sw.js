f9630568a4c7d3856590e7698b33fdf60baa8267
Saitama21 <62865879+Saitama21@users.noreply.github.com>
2026-09-23 10:04:26 +0300

    Bump PWA cache to v0.8.1

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="828d-modes-v0.8.0";
+const CACHE="828d-modes-v0.8.1";
 const CORE=[
   "./","./index.html","./styles.css","./app.js","./manifest.webmanifest",
   "./assets/icon.svg","./assets/flange.svg","./assets/bushing.svg",
